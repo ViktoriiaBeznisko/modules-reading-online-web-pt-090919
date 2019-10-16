b056fe3002ddf2bfa6cd38de9958d0a2fca26ffc
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-16 20:36:30 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,16 +1,17 @@
-require_relative './dance_module.rb'
+#require_relative './dance_module.rb'
+#require_relative './class_methods_module.rb'
 require_relative './class_methods_module.rb'
-
 class Kid
+  
   
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
   
- # attr_accessor :name
+  attr_accessor :name
  
-#  def initialize(name)
- #   @name = name
-#  end
+  def initialize(name)
+    @name = name
+  end
   
   
   
